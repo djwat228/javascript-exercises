50f96252ae783259eff55eeb3c8e864f25d4693f
Dylan Watson <djwat228@gmail.com>
2024-02-25 15:26:17 +1100

    exercise 2 complete

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,14 @@
-const repeatString = function() {
+const repeatString = function(string, num) {
+    if(num < 0) {
+        return "ERROR";
+    };
+    
+    let value = "";
+    for (count = 1; count <= num; count++) {
+        value += string;
+    };
 
+    return value;
 };
 
 // Do not edit below this line
